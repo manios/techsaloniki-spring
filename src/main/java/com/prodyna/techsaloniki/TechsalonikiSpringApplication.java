e1486f23a6f544674a83bebfd7b47dd5a06d6012
Christos Manios <maniopaido@gmail.com>
2019-10-04 15:36:32 +0300

    Initial commit

---
+++ src/main/java/com/prodyna/techsaloniki/TechsalonikiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.prodyna.techsaloniki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsalonikiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsalonikiSpringApplication.class, args);
+	}
+
+}
