3ec35aa6cec9ad820e2e8bb33c4e9fd9152ce775
Christos Manios <maniopaido@gmail.com>
2019-10-04 16:48:06 +0300

    Add solution.

--- src/main/java/com/prodyna/techsaloniki/repository/StudentRepository.java
+++ src/main/java/com/prodyna/techsaloniki/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.prodyna.techsaloniki.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.prodyna.techsaloniki.model.Student;
@@ -9,4 +11,6 @@ public interface StudentRepository extends CrudRepository<Student, Long> {
 	// Basic CRUD is AUTO IMPLEMENTED by Spring into a Bean created during
 	// deployment.
 	// CRUD refers Create, Read, Update, Delete
+
+	List<Student> findByNameIsLikeOrSurnameIsLike(String name, String surname);
 }
